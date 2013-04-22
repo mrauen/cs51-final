e8ba9d6229c85036efcfbbc64f651bf2edfb7486
Sitan Chen <sitanchen@college.harvard.edu>
2013-04-21 22:22:18 -0400

    copied and pasted matt's emailed/fb msg'd code

--- finalspec/fibonacciheap.py
+++ finalspec/fibonacciheap.py
@@ -8,6 +8,12 @@
 # heap[0] = min root
 # heap[1] = list of roots
 
+def empty_heap:
+[None, []];
+
+def insert_new_pair(key, value, heap):
+   insert([key, value, 1, None, false, None], heap);
+
 def find_min(heap):
   return heap[0];
 
@@ -24,7 +30,7 @@ def bigger_key(node1, node2):
     return node1;
 
 def merge(heap1, heap2):
-  return [smaller_key(node1, node2)[0], heap1[1]+heap2[1]];
+  return [smaller_key(heap1[0], heap2[0]), heap1[1]+heap2[1]];
 
 def insert(node, heap):
   return merge([node, [node]], heap);
@@ -37,23 +43,24 @@ def consolidate(heap):
   while True:
     counter = 0;
     for node1, node2 in heap[1]:
-      if node1 != node2 & node1[2] == node2[2]:
+      if node1 != node2 && node1[2] == node2[2]:
         small_node = smaller_key(node1, node2);
         big_node = bigger_key(node1, node2);
         heap = remove(big_node, heap);
         small_node[2] = small_node[2] * 2;
         small_node[3] = small_node[3].append(big_node)
+        big_node[5] = small_node;
         counter = 1;
         break;
-    if counter == 0:
+    if counter = 0:
       break;
   return heap;
 
-def find_min(heap):
-  min = heap[1][1];
+def find_new_min(heap):
+  min = heap[1][0];
   for node in heap[1]:
     min = smaller_key(min, node);
-  return min
+ return min;
 
 def extract_min(heap):    
   small_node = find_min(heap);
@@ -61,22 +68,25 @@ def extract_min(heap):
   for node in small_node[3]:
     heap = insert(node, heap);
   heap = consolidate(heap);
-  heap[0] = find_min(heap);
-  return heap[0];  
+  heap[0] = find_new_min(heap);
+  return [small_node[0], small_node[1]];  
 
 def cut(node, heap):
-  node[5][3].remove(node);
-  if node[5][4]:
-    node[5][4] = False;
-    heap = cut(node[5], heap);
+  parent_node = node[5];
+  parent_node[3].remove(node);
+  parent_node[2] = parent_node[2] - node[2];
+  if parent_node[4] & parent_node[5] != None:
+    parent_node[4] = False;
+    node[5] = None;
+    heap = cut(parent_node, heap);
   else:
-    node[5][4] = True;
+    parent_node[4] = True;
+  heap = insert(node, heap);
   return heap;
 
 def decrease_key(node, new_key, heap):
   node[0] = new_key;
-  if node[5] != None & node[5][0] > new_key:
-    heap = insert(node, heap);
+  if node[5] != None && node[5][0] > new_key:
     cut(node, heap);
   return heap;
 
