325c6f226d2bfd4bb5d807679ef8733c48f49042
Sitan Chen <sitanchen@college.harvard.edu>
2013-05-01 23:23:57 -0400

    fixing fibheap and pathfinder, almost done

--- finalcode/fibonacciheap.py
+++ finalcode/fibonacciheap.py
@@ -9,31 +9,35 @@
 # heap[1] = list of roots
 
 def empty_heap():
-  [None, []]
+  return [None, []]
 
 def is_empty(heap):
-  heap[0] == None and heap[1] == []
+  return heap[0] == None and heap[1] == []
 
 def insert_new_pair(key, value, heap):
-   insert([key, value, 1, None, False, None], heap)
+   return insert([key, value, 1, None, False, None], heap)
 
 def find_min(heap):
   return heap[0];
 
 def smaller_key(node1, node2):
-  if (node1[0] < node2[0]):
+  if node2 == None:
+    return node1;
+  if (node1[1] < node2[1]):
     return node1;
   else:
     return node2;
 
 def bigger_key(node1, node2):
-  if (node1[0] < node2[0]):
+  if node2 == None:
+    return node1;
+  if (node1[1] < node2[1]):
     return node2;
   else:
     return node1;
 
 def merge(heap1, heap2):
-  return [smaller_key(heap1[0], heap2[0]), heap1[1]+heap2[1]];
+  return [(smaller_key(heap1[0], heap2[0])), (heap1[1]+heap2[1])];
 
 def insert(node, heap):
   return merge([node, [node]], heap);
@@ -45,31 +49,36 @@ def remove(node, heap):
 def consolidate(heap):
   while True:
     counter = 0;
-    for node1, node2 in heap[1]:
-      if node1 != node2 and node1[2] == node2[2]:
-        small_node = smaller_key(node1, node2);
-        big_node = bigger_key(node1, node2);
-        heap = remove(big_node, heap);
-        small_node[2] = small_node[2] * 2;
-        small_node[3] = small_node[3].append(big_node)
-        big_node[5] = small_node;
-        counter = 1;
-        break;
+    if heap[1] != None:
+      for node1, node2 in heap[1]:
+        if node1 != node2 and node1[2] == node2[2]:
+          small_node = smaller_key(node1, node2);
+          big_node = bigger_key(node1, node2);
+          heap = remove(big_node, heap);
+          small_node[2] = small_node[2] * 2;
+          small_node[3] = small_node[3].append(big_node)
+          big_node[5] = small_node;
+          counter = 1;
+          break;
     if counter == 0:
       break;
   return heap;
 
 def find_new_min(heap):
-  min = heap[1][0];
-  for node in heap[1]:
-    min = smaller_key(min, node);
-  return min
+  if heap[1] == None:
+    return None;
+  else:
+    min = heap[1][0];
+    for node in heap[1]:
+      min = smaller_key(min, node);
+    return min
 
 def extract_min(heap):    
   small_node = find_min(heap);
   heap = remove(small_node, heap);
-  for node in small_node[3]:
-    heap = insert(node, heap);
+  if small_node[3] != None: 
+    for node in small_node[3]:
+      heap = insert(node, heap);
   heap = consolidate(heap);
   heap[0] = find_new_min(heap);
   return [small_node[0], small_node[1]];  
--- finalcode/pathfinder.py
+++ finalcode/pathfinder.py
@@ -6,34 +6,35 @@ import fileinput
 import fibonacciheap
 import graph
 
-def pathfinder (self, item_id, useritem_ids):
-  dist = {item_id : 0}
+def pathfinder (self, item, useritem_ids):
+  dist = {}
   prev = {}
-  Q = fibonacciheap.empty_heap
-  fibonacciheap.insert_new_pair(item_id, 0, Q)
+  Q = fibonacciheap.empty_heap()
+  Q=fibonacciheap.insert_new_pair(item, 0, Q)
   for v in self.item_list:
-    dist[v.id] = -1
-    prev[v.id] = None
-  iterator = start
-  while(fibonacciheap.is_empty(Q)):
+    dist[v] = 1000000
+    prev[v] = None
+  dist[item] = 0
+  while(not fibonacciheap.is_empty(Q)):
     u = fibonacciheap.extract_min(Q)
-    if dist[u.id] == -1:
+    if dist[u[1]] == 1000000:
       break
     for edge in u.edge_list:
       alt = dist[u.id] + math.log(edge.conf)
       if alt < dist[edge.neighbor_id]:
+
         dist[edge.neighbor_id] = alt
-        prev[edge.neighbor_id] = u;
-        fibonacciheap.insert_new_pair(edge.neighbor_id, dist[edge.neighbor_id],Q)
+        prev[edge.neighbor_id] = u.id;
+        Q = fibonacciheap.insert_new_pair(edge.neighbor_id, dist[edge.neighbor_id],Q)
   M = 0
-  candidate = useritem_ids[0]
+  candidate = useritem_ids[0].id
   for item in useritem_ids:
-    if(dist[item] > M):
-      M = dist[item]
-      candidate = item
+    if(dist[item.id] > M):
+      M = dist[item.id]
+      candidate = item.id
   cursor = prev[candidate]
   diff = 0
-  while(cursor2 != item):
+  while(cursor != item.id):
     tmp = prev[cursor]
     info = get_edge(tmp,cursor)
     diff += info[0]*info[1]
@@ -46,8 +47,8 @@ else:
   f = open(sys.argv[1], 'r')
   N = int(float(f.readline()))
   vs = []
-  for n in range(0,N):
-    vs.append(graph.Item(n,0,"",0,0,{}))
+  for n in range(1,N+1):
+    vs.append(graph.Item(n,"","",0,0,0,0,[],False))
   counter = 0
   while True:
     line1 = f.readline()
@@ -68,5 +69,5 @@ else:
       e = graph.Edge(vs[edge], None, diff, 0, num, conf)
       vs[i].edge_list.append(e)
   g = graph.Graph("test", vs)
-  answer = pathfinder(g, vs[0], [vs[3]])
+  answer = pathfinder(g, vs[0].id, [vs[3]])
   print(answer)
